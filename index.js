9a63a746392027d81056101e5086ea31b01e33c6
AnkitIITR <ankit_y@ar.iitr.ac.in>
2024-03-30 20:30:17 +0530

    code change

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ const io = new Server(server, {
   },
 });
 
-
-
 app.get("/", (req, res) => {
   return res.json({
     message: "server is running",
@@ -57,62 +55,65 @@ app.use("/user", userRouter);
 
 //middleware for socket auth check
 io.use(async (socket, next) => {
-    console.log("used");
-    console.log(socket.request.headers);
+//   console.log("used");
+//   console.log(socket.request.headers);
 
-    // Check if the cookie exists in the request
-    if (socket.request.headers.cookie) {
-        const token = socket.request.headers.cookie.split("; ").find(cookie => cookie.startsWith("token="));
-        if (token) {
-            const authToken = token.split("=")[1];
-            // User is authenticated, proceed with the connection
-            try {
-                const decodetoken = jwt.verify(authToken, process.env.JWT_SECRET);
-                const user = await User.findById(decodetoken._id);
-                if (user) {
-                    socket.user = user;
-                    return next();
-                }
-            } catch (error) {
-                console.error("Error verifying token:", error);
-            }
+  // Check if the cookie exists in the request
+  if (socket.request.headers.cookie) {
+    const token = socket.request.headers.cookie
+      .split("; ")
+      .find((cookie) => cookie.startsWith("token="));
+    if (token) {
+      const authToken = token.split("=")[1];
+      // User is authenticated, proceed with the connection
+      try {
+        const decodetoken = jwt.verify(authToken, process.env.JWT_SECRET);
+        const user = await User.findById(decodetoken._id);
+        if (user) {
+          socket.user = user;
+          return next();
         }
+      } catch (error) {
+        console.error("Error verifying token:", error);
+      }
     }
-    // User is not authenticated, reject the connection
-    return next(new Error("Unauthorized"));
+  }
+  // User is not authenticated, reject the connection
+  return next(new Error("Unauthorized"));
 });
 
 io.on("connection", (socket) => {
-  console.log("new user is connected", socket.id);
+//   console.log("new user is connected", socket.id);
 
   const sendWelcomeMessages = () => {
     socket.emit("welcome", "Hi there!👋");
     setTimeout(() => {
-      socket.emit("welcome","I'm Wysa- an AI chatbot built by therapists.");
+      socket.emit("welcome", "I'm Wysa- an AI chatbot built by therapists.");
     }, 1000);
     setTimeout(() => {
       socket.emit(
-        "welcome","I'm here to understand your concerns and connect you with the best resources available to support you"
+        "welcome",
+        "I'm here to understand your concerns and connect you with the best resources available to support you"
       );
     }, 2000);
     setTimeout(() => {
-      socket.emit("welcome","Can I help?");
+      socket.emit("welcome", "Can I help?");
     }, 3000);
   };
 
   sendWelcomeMessages();
 
   socket.on("user-query", (msg) => {
-    console.log("User query received:", msg);
-    socket.emit("backend-response", msg);
+    // console.log("User query received:", msg);
+    setTimeout(() => {
+      socket.emit("backend-response", msg);
+    }, 3000);
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 });
-
-
 
 server.listen(PORT, () => {
   console.log(`server started on ${PORT}`);
